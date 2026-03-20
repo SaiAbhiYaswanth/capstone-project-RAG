4d931dce356ffae33b1f0feafb4ced24fc5e2117
Sai Abhi Yaswanth <asaiabhiyaswanth@gmail.com>
2026-03-20 14:37:58 +0530

    Fix import for text splitter (deployment fix)

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from config import CHUNK_SIZE, CHUNK_OVERLAP
 
 
